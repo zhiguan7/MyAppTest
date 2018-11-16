d2704ec945bf52687de3ebec897ba814a2eff304
zhiguan7 <zhiguan7@gmail.com>
2018-11-16 10:32:28 +0800

    sun

--- BroadcastPractice/app/src/main/java/com/example/wind/broadcastpractice/LoginActivity.java
+++ BroadcastPractice/app/src/main/java/com/example/wind/broadcastpractice/LoginActivity.java
@@ -1,29 +1,47 @@
 package com.example.wind.broadcastpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class LoginActivity extends BaseActivity{
 
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+
+    private CheckBox rememberpass;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
         accountEdit = (EditText) findViewById(R.id.email);
         passwordEdit  =(EditText) findViewById(R.id.password);
-
+        rememberpass = (CheckBox) findViewById(R.id.remember_pass);
         login = (Button) findViewById(R.id.email_sign_in_button);
+        boolean isRemember = preferences.getBoolean("remember_password", false);
+        if (isRemember){
+            String account = preferences.getString("account", "");
+            String password = preferences.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberpass.setChecked(true);
+        }
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,6 +52,15 @@ public class LoginActivity extends BaseActivity{
                     Toast.makeText(LoginActivity.this, "Password is null!", Toast.LENGTH_SHORT).show();
                 } else{
                     if (account.equals("admin") && password.equals("123456")){
+                        editor = preferences.edit();
+                        if (rememberpass.isChecked()){
+                            editor.putBoolean("remember_password", true);
+                            editor.putString("account", account);
+                            editor.putString("password", password);
+                        } else {
+                            editor.clear();
+                        }
+                        editor.apply();
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
                         finish();
